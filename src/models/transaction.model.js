988d10c4348ce020800724b7f5b6d0dc58b5062e
ratulislam365 <t.rautl.islma.101@gmail.com>
2025-11-15 06:17:17 +0600

    some changes

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -4,7 +4,7 @@ const transactionSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   amount: { type: Number, required: true },
   paymentMethod: { type: String, enum: ["stripe", "card", "cash"], required: true },
-  status: { type: String, enum: ["completed", "pending"], default: "pending" },
+  status: { type: String, enum: ["completed", "pending"], default: "pending" },  
   createdAt: { type: Date, default: Date.now }
 });
 
